a174c5df03a1324d5b1aebaf23e26a874259a3f2
ThaumicMekanism <thaumicmekanism@gmail.com>
2018-11-17 18:06:01 -0800

    Fixed python 2 bug with listdir with no args

--- my_tests/circ_files/modular_test.py
+++ my_tests/circ_files/modular_test.py
@@ -11,7 +11,7 @@ testEnd = ".circ"
 
 tests = []
 
-for filename in os.listdir():
+for filename in os.listdir(os.getcwd()):
     if filename.startswith(testTag) and filename.endswith(testEnd):
         testname = (filename[len(testTag):])[:-len(testEnd)]
         tests.append(("CPU " + testname + " test",AbsoluteTestCase(os.path.join(file_locations,'CPU-' + testname + '.circ'), os.path.join(file_locations,'output/CPU-' + testname + '.out'), os.path.join(file_locations,'reference_output/CPU-' + testname + '.out'),1)))
